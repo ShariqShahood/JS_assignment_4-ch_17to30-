45d857dd9c6ce1479333dad7bd231373d184b33c
SHARIQ SHAHOOD <shariqshahood43@gmail.com>
2023-12-09 09:32:23 -0800

    mini change

--- app.js
+++ app.js
@@ -22,14 +22,46 @@
 
 
                     ///Pallingdrome
- var input = prompt("Enter Any String: ");
- var data = '';  
- for (var i = input.length - 1; i >= 0; i--) {
-        data += input[i];
+//  var input = prompt("Enter Any String: ");
+//  var data = '';  
+//  for (var i = input.length - 1; i >= 0; i--) {
+//         data += input[i];
+// }
+// if (data === input) {
+//     alert(input + " is a Palindrome");
+// }
+// else {
+//     alert(input + " is Not a Palindrome");
+//     }
+
+                        // EVEN/ODD method :01
+
+// var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+// var evenArray = [];
+// var oddArray = [];
+// for (var i = 0; i < numberArray.length; i++) {
+//     if (numberArray[i] % 2 === 0) {
+//         evenArray.push(numberArray[i]);
+//     } else {
+//         oddArray.push(numberArray[i]);
+//     }
+// }
+// console.log("Even Numbers: " + evenArray);
+// console.log("Odd Numbers: " + oddArray);
+
+                        // EVEN/ODD method :02
+
+var input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+var Even = [];
+var Odd = [];
+for (var i=0; i<input.length; i++){
+if (input[i] % 2 === 0){
+    Even.push(input[i]);
 }
-if (data === input) {
-    alert(input + " is a Palindrome");
+else{
+    Odd.push(input[i]);
 }
-else {
-    alert(input + " is Not a Palindrome");
-    }
+
+}
+document.write("<h1> Even is:  {" + Even +"}</h1><br>");
+document.write("<h1> Odd is:  {" + Odd+"}</h1>");
